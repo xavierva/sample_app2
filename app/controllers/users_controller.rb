2701e4760c806e961590b8b32de7cdaeaa52e8b2
Xavier <vonaarburgx@gmail.com>
2012-12-24 01:54:09 +0100

    8.4.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
     @titre = "Inscription"
   end
   
   def show
     @user = User.find(params[:id])
     @titre = @user.nom
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Bienvenue dans l'application Boloss!"
+      redirect_to @user
+    else
+      @titre = "Inscription"
+      render 'new'
+    end
   end
   
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,20 +38,74 @@ describe UsersController do
 
   end
   
-  Describe "GET 'new'" do
+  describe "GET 'new'" do
     
     it "devrait réussir" do
       get :new
-      response.shoud be_success
+      response.should be_success
     end
     
     it "devrait avoir le bon titre" do
       get :new
       response.should have_selector("title", :content => "Inscription")
     end
+    
+   end
+   
+    describe "POST 'create'" do
+      
+      describe "échec" do
+         
+         before(:each) do
+            @attr = { :nom => "", :email => "", :password => "",
+                      :password_confirmation => "" }
+         end
   
+         it "ne devrait pas créer d'utilisateur" do
+           lambda do
+             post :create, :user => @attr
+           end.should_not change(User, :count)
+         end
+
+         it "devrait avoir le bon titre" do
+           post :create, :user => @attr
+           response.should have_selector("title", :content => "Inscription")
+         end
+
+         it "devrait rendre la page 'new'" do
+           post :create, :user => @attr
+           response.should render_template('new')
+         end
+      end #fin échec
+      
+      
+        describe "succès" do
+
+           before(:each) do
+              @attr = { :nom => "New User", :email => "user@example.com",
+                        :password => "foobar", :password_confirmation => "foobar" }
+           end
+
+           it "devrait créer un utilisateur" do
+              lambda do
+                post :create, :user => @attr
+              end.should change(User, :count).by(1)
+           end
+
+           it "devrait rediriger vers la page d'affichage de l'utilisateur" do
+              post :create, :user => @attr
+              response.should redirect_to(user_path(assigns(:user)))
+           end
+           
+           it "devrait avoir un message de bienvenue" do
+              post :create, :user => @attr
+              flash[:success].should =~ /Bienvenue dans l'application Boloss/i
+           end
+           
+        end #fin succès
+         
   
-  end
+    end #Fin POST 'create'
   
   
     
